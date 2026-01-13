174b093f04bcaf068404f288fc81c90b6ed3ee33
HYUN <hanm9el@naver.com>
2026-01-13 13:17:02 +0900

    MainVisual 작성 준비

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { twMerge } from "tailwind-merge";
+import MainVisual from "../components/home/MainVisual.tsx";
 
 function Home() {
-  return <div className={twMerge(["h-[200dvh]","bg-green-500"])}></div>
+  return <div className={twMerge(["min-h-screen", "flex","flex-col"])}>
+    <MainVisual />
+  </div>
 }
 
 export default Home;
